2334c57a9ca6ade3b520142ed9dd4b9d90c4966d
Dauren Muratov <dauren@fb.com>
2022-04-22 23:59:49 +0100

    add git aliases and tmux config

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,6 @@
 set fish_greeting ""
 
 # aliases
-alias g git
 
 # nvim
 if type -q nvim
@@ -13,6 +12,21 @@ if type -q exa
   alias ls "exa"
   alias ll "exa -l"
   alias la "ll -a"
+end
+
+# git
+if type -q git 
+  alias g git
+	alias ga "g add"
+	alias gaa "g add ."
+	alias gc "g commit"
+	alias gclean "g reset --hard"
+	alias gs "g status --short"
+	alias gss "g status"
+	alias go "g checkout"
+	alias gg "g pull"
+	alias gd "g diff"
+	alias gl "g log --graph --pretty=format:'%Cred%h%Creset %C(bold blue)%an%C(reset) - %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 end
 
 if status is-interactive
