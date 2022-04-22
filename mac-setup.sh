f88eb46c264e41731e7d37738dc57ca96a8e379a
Dauren <dauren.ktl@gmail.com>
2022-04-22 12:31:03 +0100

    add mac-setup script

---
+++ mac-setup.sh
@@ -0,0 +1,12 @@
+echo "Disable press-and-hold for keys in favor of key repeat"
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
+echo "Set a blazingly fast keyboard repeat rate"
+defaults write NSGlobalDomain KeyRepeat -int 1
+
+echo "Set a shorter Delay until key repeat"
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
+
+echo "Enable tap to click (Trackpad)"
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+
